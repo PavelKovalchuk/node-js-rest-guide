fae32e034b487314567832b813a96bfa85346ca1
PavelKovalchuk <pavel.kovalchuk@dataart.com>
2019-10-04 17:38:19 +0300

    409/ delete posts via socket

--- controllers/feed.js
+++ controllers/feed.js
@@ -11,7 +11,7 @@ exports.getPosts = async (req, res, next) => {
   const perPage = 2;
   try {
     const totalItems = await Post.find().countDocuments();
-    const posts = await Post.find().populate("creator").skip((currentPage - 1) * perPage).limit(perPage);
+    const posts = await Post.find().populate("creator").sort({createdAt: -1}).skip((currentPage - 1) * perPage).limit(perPage);
     res.status(200).json({
       message: 'Fetched posts successfully.',
       posts: posts,
@@ -140,7 +140,7 @@ exports.updatePost = async (req, res, next) => {
 
     io.getIO().emit('posts', { action: 'update', post: result });
     res.status(200).json({ message: 'Post updated!', post: result });
-    
+
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -150,40 +150,40 @@ exports.updatePost = async (req, res, next) => {
 };
 
 
-exports.deletePost = (req, res, next) => {
+exports.deletePost = async (req, res, next) => {
   const postId = req.params.postId;
-  Post.findById(postId)
-    .then(post => {
-      if (!post) {
-        const error = new Error('Could not find post.');
-        error.statusCode = 404;
-        throw error;
-      }
-      if (post.creator.toString() !== req.userId) {
-        const error = new Error('Not authorized.');
-        error.statusCode = 403;
-        throw error;
-      }
 
-      clearImage(post.imageUrl);
-      return Post.findByIdAndRemove(postId);
-    })
-    .then(result => {
-      return User.findById(req.userId);
-    })
-    .then(user => {
-      user.posts.pull(postId);
-      return user.save();
-    })
-    .then(result => {
-      res.status(200).json({ message: 'Deleted post.' });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      const error = new Error('Could not find post.');
+      error.statusCode = 404;
+      throw error;
+    }
+    if (post.creator.toString() !== req.userId) {
+      const error = new Error('Not authorized!');
+      error.statusCode = 403;
+      throw error;
+    }
+    // Check logged in user
+    clearImage(post.imageUrl);
+    await Post.findByIdAndRemove(postId);
+
+    const user = await User.findById(req.userId);
+    user.posts.pull(postId);
+    await user.save();
+
+    io.getIO().emit('posts', { action: 'delete', post: postId });
+    res.status(200).json({ message: 'Deleted post.' });
+
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+ 
 };
 
 const clearImage = filePath => {
